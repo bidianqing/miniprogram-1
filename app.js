8abc813be7dadd48170d7bdb461338a84c74777b
bidianqing <bidianqing@qq.com>
2023-06-24 01:45:13 +0800

    up

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // app.js
 App({
   onLaunch() {
+    let self = this;
     // 展示本地存储能力
     // const logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
+    wx.cloud.init({
+      env: 'development'
+    })
+    self.globalData.db = wx.cloud.database({
+      env: 'development-9gisgsae4eece201'
+    })
     
     // 登录
     wx.login({
@@ -12,8 +19,7 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-
-    let self = this;
+    
     self.start();
 
     wx.onSocketClose(function(){
@@ -22,15 +28,52 @@ App({
       self.start();
     })
 
+    wx.onSocketError(function(){
+      console.log('error');
+      self.globalData.isConnected = 0;
+      self.start();
+    })
 
-
+    wx.onSocketMessage(function(message){
+      var obj = JSON.parse(message.data.replace('',''));
+      console.log(obj);
+      // 根据方法名字，执行方法
+      if(obj.target){
+        self[obj.target](obj.arguments);
+      }
+    })
+  },
+  logout(arrayArg){
+    let self = this;
+    wx.closeSocket();
+    self.globalData.logout = 1;
+    console.log(arrayArg);
+  },
+  receiveOffLineMessage(arrayArg){
+    let self = this;
+    console.log(arrayArg);
+  },
+  receiveSingleMessage(arrayArg){
+    let self = this;
+    console.log(arrayArg);
+    self.globalData.db.collection('tb_conversation').add({
+      data:{
+        fromUserId: arrayArg[0],
+        content: arrayArg[1],
+        sendTime: new Date()
+      }
+    })
   },
   start(){
     let self = this;
     console.log('WebSocket连接状态：' + self.globalData.isConnected);
-    if(self.globalData.isConnected === 0){
+    if(self.globalData.isConnected === 0 && self.globalData.logout === 0){
       wx.connectSocket({
         url: 'wss://bidianqing.natappvip.cc/chathub',
+        header:{
+          "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTExNzgyNTY2Mzg1MjY0MjMwNCIsIm5iZiI6MTY4NzQ0MzA4MCwiZXhwIjoxNjg5OTY5NjAwLCJpc3MiOiJjdXBpZGNvbWVvcmciLCJhdWQiOiJjY3VzZXIifQ.teYSQtgjX_Hhsw3V_4z_-mg82b5Xi3m4IuRNZS8SPaE",
+          "Platform":"miniprogram"
+        },
         success: function(){
           console.log('connectSocket调用成功');
         }
@@ -48,5 +91,7 @@ App({
   globalData: {
     userInfo: null,
     isConnected: 0,
+    logout: 0,
+    db: null
   }
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -12,22 +12,5 @@ Page({
       logs : wx.getStorageSync('logs') || []
     })
     console.log('log.js onLoad');
-    wx.onSocketMessage(function(message){
-      var obj = JSON.parse(message.data.replace('',''));
-      console.log(obj);
-      // 根据方法名字，执行方法
-      if(obj.target){
-        self[obj.target](obj.arguments);
-      }
-    })
-    
-  },
-  receiveMessage(arg){
-    let self = this;
-    console.log(arg[0]);
-    self.data.logs.push({ message:arg[0]});
-    self.setData({
-      logs : self.data.logs
-    })
   }
 })
