44564cb1b6e5a7b5e1c1f7692db92621ec2a1105
bidianqing <bidianqing@longfor.com>
2023-11-27 16:00:54 +0800

    *

--- app.js
+++ app.js
@@ -2,34 +2,17 @@
 App({
   onLaunch() {
     let self = this;
-    // 展示本地存储能力
-    // const logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-    wx.cloud.init({
-      env: 'development'
-    })
-    self.globalData.db = wx.cloud.database({
-      env: 'development-9gisgsae4eece201'
-    })
-    
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     
     self.start();
 
     wx.onSocketClose(function(){
-      console.log('close');
+      console.log('onSocketClose');
       self.globalData.isConnected = 0;
       self.start();
     })
 
     wx.onSocketError(function(){
-      console.log('error');
+      console.log('onSocketError');
       self.globalData.isConnected = 0;
       self.start();
     })
@@ -43,37 +26,16 @@ App({
       }
     })
   },
-  logout(arrayArg){
+  receiveMessage(message){
     let self = this;
-    wx.closeSocket();
-    self.globalData.logout = 1;
-    console.log(arrayArg);
-  },
-  receiveOffLineMessage(arrayArg){
-    let self = this;
-    console.log(arrayArg);
-  },
-  receiveSingleMessage(arrayArg){
-    let self = this;
-    console.log(arrayArg);
-    self.globalData.db.collection('tb_conversation').add({
-      data:{
-        fromUserId: arrayArg[0],
-        content: arrayArg[1],
-        sendTime: new Date()
-      }
-    })
+    console.log('receiveMessage方法被调用，入参：' + message);
   },
   start(){
     let self = this;
     console.log('WebSocket连接状态：' + self.globalData.isConnected);
-    if(self.globalData.isConnected === 0 && self.globalData.logout === 0){
+    if(self.globalData.isConnected === 0){
       wx.connectSocket({
-        url: 'wss://bidianqing.natappvip.cc/chathub',
-        header:{
-          "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTExNzgyNTY2Mzg1MjY0MjMwNCIsIm5iZiI6MTY4NzQ0MzA4MCwiZXhwIjoxNjg5OTY5NjAwLCJpc3MiOiJjdXBpZGNvbWVvcmciLCJhdWQiOiJjY3VzZXIifQ.teYSQtgjX_Hhsw3V_4z_-mg82b5Xi3m4IuRNZS8SPaE",
-          "Platform":"miniprogram"
-        },
+        url: 'wss://bidianqing.cn/chathub',
         success: function(){
           console.log('connectSocket调用成功');
         }
@@ -81,7 +43,7 @@ App({
     }
 
     wx.onSocketOpen(function(){
-      console.log('握手');
+      console.log('开始握手');
       self.globalData.isConnected = 1;
       wx.sendSocketMessage({
         data: '{"protocol":"json","version":1}'
