217f94554cfbea5eeb4a6a18c0e1f75de819153a
bidianqing <bidianqing@qq.com>
2023-11-29 21:48:49 +0800

    *

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,6 +1,7 @@
 // index.js
 // 获取应用实例
 const app = getApp()
+import Notify from '@vant/weapp/notify/notify';
 
 Page({
   data: {
@@ -9,5 +10,7 @@ Page({
   onShow(){
     let self = this;
     self.getTabBar().setData({ active : 2 })
+    wx.vibrateLong();
+    Notify({ type: 'success', message: '通知内容' });
   }
 })
